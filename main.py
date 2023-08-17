5a49120f6290c94bae5ff713d67dc881ae6df4b8
Anton-Shcherba <tony.shcherba@gmail.com>
2023-08-17 11:38:03 +0300

    feat: add echo func

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("start cmd")
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
