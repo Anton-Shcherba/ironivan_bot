201b34899d1b338e4665679c6a2f1ab512c329d5
Anton-Shcherba <tony.shcherba@gmail.com>
2023-08-25 16:46:02 +0300

    feat: add basic server communication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
-function App(): JSX.Element {
+const socket = io('ws://localhost:5678');
 
-  const { user } = window.Telegram.WebApp.initDataUnsafe
+function App(): JSX.Element {
+  // const { user } = window.Telegram.WebApp.initDataUnsafe
+  const [messages, setMessages] = useState<any[]>([]);
+
+  useEffect(() => {
+    socket.on('message', (data) => {
+      console.log(data)
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+  }, []);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const messageInput = event.currentTarget.elements.namedItem('message') as HTMLInputElement;
+    if (messageInput) {
+      const message = messageInput.value;
+      socket.emit('message', message);
+      messageInput.value = '';
+    }
+  };
 
   return (
     <div className="App">
-      <p>{user?.id ? user.id : 'bad_id'}</p>
+      {/* <p>{user?.id ? user.id : 'bad_id'}</p> */}
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="message" />
+        <button>Отправить</button>
+      </form>
     </div>
   );
 }
