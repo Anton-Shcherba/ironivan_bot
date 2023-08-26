a267326c33a4daf3653a2b240976642bcac53973
Anton-Shcherba <tony.shcherba@gmail.com>
2023-08-26 17:03:22 +0300

    build: install mui & styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 function App(): JSX.Element {
-
-  const { user } = window.Telegram.WebApp.initDataUnsafe
+  const { user } = window.Telegram.WebApp.initDataUnsafe;
 
   return (
     <div className="App">
       <p>{user?.id ? user.id : 'bad_id'}</p>
+      <div></div>
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 }
